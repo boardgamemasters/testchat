b53189b9c5a03b4d53ad8607c77963353a21d366
Frederik Loy <130966694+Loy-Frederik@users.noreply.github.com>
2023-07-30 16:04:26 +0200

    Update steamchat.py

--- steamchat.py
+++ steamchat.py
@@ -13,12 +13,12 @@ test_items = [
         title="Slide 1",
         text="A tree in the savannah",
         interval=None,
-        img="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=1380&t=st=1688825493~exp=1688826093~hmac=cb486d2646b48acbd5a49a32b02bda8330ad7f8a0d53880ce2da471a45ad08a4",
+        img="https://cf.geekdo-images.com/EVfMwPiHmxDUvY32BbghBg__imagepage/img/vFmU3UHOT8CT0ssjOw4OFmwR2jA=/fit-in/900x600/filters:no_upscale():strip_icc()/pic7378384.jpg",
     ),
     dict(
         title="Slide 2",
         text="A wooden bridge in a forest in Autumn",
-        img="https://img.freepik.com/free-photo/beautiful-wooden-pathway-going-breathtaking-colorful-trees-forest_181624-5840.jpg?w=1380&t=st=1688825780~exp=1688826380~hmac=dbaa75d8743e501f20f0e820fa77f9e377ec5d558d06635bd3f1f08443bdb2c1",
+        img="https://cf.geekdo-images.com/x3zxjr-Vw5iU4yDPg70Jgw__imagepage/img/-17KkOmxbTu2slJTabGrkO8ZW8s=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3490053.jpg",
     ),
     dict(
         title="Slide 3",

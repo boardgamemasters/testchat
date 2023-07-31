f3e17ac692b4c2b6a3fd9453daeba81e17297f0d
LeilaKarimii <131779385+LeilaKarimii@users.noreply.github.com>
2023-07-31 20:50:56 +0200

    Update chat-not-sensetive.py

--- chat-not-sensetive.py
+++ chat-not-sensetive.py
@@ -1,118 +1,81 @@
 import streamlit as st
-import pandas as pd
 from streamlit_chat import message
 
-games = pd.Series(['lama', 'cow', 'lama', 'beetle', 'lama', 'hippo'])
+# The rest of your code...
 
-def on_input_change():
-    user_input = st.session_state.user_input
-    st.session_state.responses.append(user_input)
-    st.session_state.user_input = ""  # Clear the input after processing
-
-def on_btn_click():
-    del st.session_state['questions']
-    del st.session_state['responses']
+#with st.container():
+with st.sidebar(): # Place the chatbot code inside the sidebar
     selecthor = 0
-
-st.session_state.setdefault('questions', [])
-
-# Add custom CSS to position the chatbot in the bottom right corner and size it as Samsung S22 mobile
-st.markdown("""
-<style>
-    .stApp {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-end;
-        align-items: flex-start;
-        height: 80vh;
-        width: 80vw;
-        overflow: hidden;
-    }
-
-    .stApp > div:first-child {
-        width: 375px; 
-        height: 860px; 
-        max-height: calc(100vh - 40px); 
-        overflow-y: auto;
-        border: 1px solid black;
-        border-radius: 10px;
-        background-color: #f0f0f0; 
-        padding: 10px;
-        margin-bottom: 20px;
-</style>
-""", unsafe_allow_html=True)
-
-st.title("Survey QA Bot")
-questions_list = [
-    # 0
-    '''I would like to recommend you some Boardgames.
-    What is your favorite one?'''    
-    # 1
-    , '''I dont know this Game.
-    Please enter another one'''
-    # 2
-    , '''How many recommendations do you want to get?
-    'Please enter a Number between 1 and 5'''
-]
-
-if 'responses' not in st.session_state.keys():
-    st.session_state.questions.extend(questions_list)
-    st.session_state.responses = []
-
-chat_placeholder = st.empty()
-st.button("Clear message", on_click=on_btn_click)
-
-message(st.session_state.questions[0]) 
-
-
-# Place the chatbot code inside the main container
-with st.container():
-    st.text_input("User Response:", on_change=on_input_change, key="user_input")
-    with st.sidebar:  # Place the chatbot code inside the sidebar
-        st.markdown('<div>', unsafe_allow_html=True)
-        
-        selecthor = 0
-        count = 0
-        for response in (st.session_state.responses):
-            count += 1
-            if selecthor == 0:
-                message(response, is_user=True, key=f"a1{count}")
-                if games.str.fullmatch(response, case=False).any():
-                    if ((games.str.fullmatch(response, case=False)).sum()) != 1:
-                        sel_game = games.loc[games.str.fullmatch(response, case=False)][0]
-                    else:
-                        sel_game = games.loc[games.str.fullmatch(response, case=False)]
-                    selecthor = 1
-                    message(st.session_state.questions[2], key=f"b2{count}")  
-                    continue
-                else:
-                    message(st.session_state.questions[1], key=f"b1{count}")
-            if selecthor == 1:
-                message(response, is_user=True, key=f"a2{count}")
-                if response.isnumeric():
-                    alt = response
-                    selecthor = 3
-                    message(f'''Your favorite boardgame is {sel_game}.
-                    And you would like to get {alt} recommendations for similar games.
-                    Is that correct?
-                    (y) , (n)''', key=f"b4{count}")
-                    continue
-                else:
-                    message('Please enter a numeric value', key=f"b3{count}")
-            if selecthor == 2:
+    count =0
+    # while 1==1:
+    for response in (st.session_state.responses):
+        count +=1
+        #response = response.lower()  # Convert user response to lowercase
+        if selecthor == 0:
+            message(response, is_user = True, key=f"a1{count}")
+            if games.str.fullmatch(response, case = False).any():
+              if ((games.str.fullmatch(response, case = False)).sum())!=1:
+                sel_game = games.loc[games.str.fullmatch(response, case = False)][0]
+              else:
+                sel_game = games.loc[games.str.fullmatch(response, case = False)]
+              selecthor = 1
+              message(st.session_state.questions[2], key=f"b2{count}")  
+              continue
+            else:
+                message(st.session_state.questions[1], key=f"b1{count}")
+        if selecthor == 1:
+            # message(st.session_state.questions[2], key=f"b2{count}")
+            message(response, is_user = True, key=f"a2{count}")
+            if response.isnumeric():
+                alt = response
                 selecthor = 3
+                message(f'''Your favorite boardgame is {sel_game}.
+                And you would like to get {alt} recommendations for similar games.
+                Is that correct?
+                (y) , (n)''', key=f"b4{count}")
                 continue
-            if selecthor == 3:
-                message(response, is_user=True, key=f"a3{count}")  
-                if (pd.Series(['y', 'Y', 'yes', 'Yes'])).isin([response]).any():
-                    message('I can recommend you the following games:', key=f"b5{count}")
-                elif (pd.Series(['n', 'N', 'no', 'No'])).isin([response]).any():
-                    message('Lets try again', key=f"b6{count}")
-                    selecthor = 0
-                    continue
-                else:
-                    message(f'''{response} is not a valid input. Please try again
-                    What is your favorite Boardgame?''', key=f"b7{count}")
-                    selecthor = 0
-                    continue
-        st.markdown('</div>', unsafe_allow_html=True)
+            else:
+                message('Please enter a numeric value', key=f"b3{count}")
+        if selecthor== 2:
+            # message(f'''Your favorite boardgame is {sel_game}.
+            # And you would like to get {alt} recommendations for similar games.
+            # Is that correct?
+            # (y) , (n)''', key=f"b4{count}")
+            selecthor = 3
+            continue
+        if selecthor== 3:
+            message(response, is_user = True, key=f"a3{count}")  
+            if (pd.Series(['y', 'Y', 'yes', 'Yes'])).isin([response]).any():
+                message('I can recommend you the following games:', key=f"b5{count}")
+            elif (pd.Series(['n', 'N', 'no', 'No'])).isin([response]).any():
+                message('Lets try again', key=f"b6{count}")
+                selecthor = 0
+                continue
+            else:
+                message(f'''{response} is not a valid input. Please try again
+                What is your favorite Boardgame?''', key=f"b7{count}")
+                selecthor = 0
+                continue
+                
+                     
+                   
+    
+    # for response, question in zip(st.session_state.responses, st.session_state.questions[1:]):
+    #     message(response, )
+    #     message(response)
+    #     message(question)
+
+# Define the layout with columns and rows
+col1, col2 = st.beta_columns(2)
+
+# Place the chat input box in the first column
+with col1:
+    st.text_input("User Response:", on_change=on_input_change, key="user_input")
+
+# Place the chat output (chatbot messages) in the second column
+with col2:
+    chat_placeholder = st.empty()
+    st.button("Clear message", on_click=on_btn_click)
+
+    message(st.session_state.questions[0])
+

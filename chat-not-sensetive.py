a4bb4d1e9fb9725f2f0cc0f9f440a4451c72c5b3
LeilaKarimii <131779385+LeilaKarimii@users.noreply.github.com>
2023-07-31 16:31:56 +0200

    Update chat-not-sensetive.py

--- chat-not-sensetive.py
+++ chat-not-sensetive.py
@@ -137,7 +137,8 @@ with st.container():
     #     message(response)
     #     message(question)
 
-st.markdown('</div>', unsafe_allow_html=True)
+
 with st.container():
     st.text_input("User Response:", on_change=on_input_change, key="user_input")
+    st.markdown('</div>', unsafe_allow_html=True)
 
